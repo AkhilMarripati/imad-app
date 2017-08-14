c20e8779250514c2ac8653a5bd76224285557ceb
AkhilMarripati <30879017+AkhilMarripati@users.noreply.github.com>
2017-08-15 02:27:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemp(articleOne)));
+  res.sendFile(createTemp(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
